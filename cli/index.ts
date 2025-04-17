247f1a95740e07eda15370dc66b0c9297404ddad
rocktimsaikia <rocktimthedev@gmail.com>
2025-04-17 18:36:38 +0530

    setup args

--- cli/index.ts
+++ cli/index.ts
@@ -1,1 +1,14 @@
-console.log("Notes CLI");
+import { parse } from "@bomb.sh/args";
+
+const argv = process.argv.slice(2);
+const args = parse(argv, {
+	alias: { h: "help", v: "version" },
+	boolean: ["help", "version"],
+	array: [""],
+});
+
+if (args._.length === 0) {
+	// TODO: Show interactive prompt
+	console.error("Missing command");
+	process.exit(1);
+}
